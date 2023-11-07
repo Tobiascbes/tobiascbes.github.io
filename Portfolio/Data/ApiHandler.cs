63a09d5f71de8eba22618aca56169a79e38ff6a5
Tobias Casanas Besser <98935282+Tobiascbes@users.noreply.github.com>
2023-11-07 10:05:24 +0100

    New Portfolio Site

---
+++ Portfolio/Data/ApiHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using RestSharp;
+namespace Portfolio.Data;
+
+public class ApiHandler
+{
+    private IConfiguration _configuration;
+    public List<GithubInfo> githubInfos()
+	{
+		RestClient restClient = new("https://api.github.com/user/repos");
+		RestRequest restRequest = new();
+        restRequest.AddHeader("Authorization", $"Bearer {_configuration["BearerToken:GithubApi"]}");
+        return restClient.Get<List<GithubInfo>>(restRequest);
+    }
+    public ApiHandler(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+}
+public class GithubInfo
+{
+    public int ID { get; set; }
+    public string? Name { get; set; }
+    public string? Login { get; set; }
+    public string? Html_Url { get; set; }
+    public bool Private { get; set; }
+   
+
+    public GithubInfo(int id, string name, string login, string html_url)
+    {
+        ID = id;
+        Name = name;
+        Login = login;
+        Html_Url = html_url;
+
+        
+    }
+}
+
